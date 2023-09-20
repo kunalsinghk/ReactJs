7c3e2c526cdbe0b320e7910b9c2a1679fdc0402b
kunalsinghk <Kunalsinghk1998@gmail.com>
2023-09-20 12:30:23 +0530

    React first module

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// App.js
+import React from 'react';
+import ExpenseDetails from './components/ExpenseDetails';
+
+import './components/ExpenseItem.css'
+
+const App=()=> {
+  const expenses = [
+    {
+      id: 'e1',
+      date: new Date(2023, 9, 13),
+      title: "Food",
+      amount: 50,
+      locationOfExpenditure: "Hotel",
+    },
+    {
+      id: 'e2',
+      date: new Date(2023, 9, 13),
+      title: "Petrol",
+      amount: 100,
+      locationOfExpenditure: "Petrol Pump",
+    },
+    {
+      id: 'e3',
+      date: new Date(2023, 9, 13),
+      title: "Movies",
+      amount: 200,
+      locationOfExpenditure: "Inox",
+    }
+  ];
+
+  return (
+    <div>
+      <div className="expense-item">
+        <div className="expense-item__description">
+          <h1>Expense Item!</h1>
+        </div>
+      </div>
+      <div>
+        <ExpenseDetails expenses={expenses} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from "./card";
+
+
+const ExpenseItem=(props) =>{
+    return (       
+      <Card className="expense-item"> 
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <h2>{props.locationOfExpenditure}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>    
+  );
+}
+
+export default ExpenseItem;
