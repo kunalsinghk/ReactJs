d7615194dd163302ca24f40b05dc519e431ed9dc
kunalsinghk <Kunalsinghk1998@gmail.com>
2023-09-30 22:48:05 +0530

    Add event listner

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,6 +12,7 @@ const ExpenseItem=(props) =>{
         <h2>{props.title}</h2>
         <h2>{props.locationOfExpenditure}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button className='expense-item__delete' onClick={'deleteButton'}>Delete</button>
         </div>
       </Card>    
   );
